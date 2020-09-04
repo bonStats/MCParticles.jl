331f6d99480819110eedf405dea9671d08729eeb
Joshua Bon <joshuajbon@gmail.com>
2020-09-04 21:24:54 +1000

    Added basic construction tests for DynamicParticle

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,23 @@ using MCParticles
 using Test
 
 @testset "MCParticles.jl" begin
-    # Write your tests here.
+    # Constructor tests
+    f(x,t) = -sum(x) + t
+    dyn_p1_1 = DynamicParticle([1.0,1.0], 1.0, f, 1.0, f([1.0,1.0],1.0))
+    dyn_p1_2 = Particle([1.0,1.0], 1.0, f, 1.0)
+    @test all([
+        dyn_p1_1.x == dyn_p1_2.x,
+        dyn_p1_1.ℓw == dyn_p1_2.ℓw,
+        dyn_p1_1.ℓdens == dyn_p1_2.ℓdens,
+        dyn_p1_1.cacheℓdens == dyn_p1_2.cacheℓdens,
+        dyn_p1_1.τ == dyn_p1_2.τ])
+    g(x) = -sum(x)
+    dyn_p2_1 = DynamicParticle([1.0 1.0], 1.0, g, 1, g([1.0 1.0]))
+    dyn_p2_2 = Particle([1.0 1.0], 1.0, g, 1)
+    @test all([
+        dyn_p2_1.x == dyn_p2_2.x,
+        dyn_p2_1.ℓw == dyn_p2_2.ℓw,
+        dyn_p2_1.ℓdens == dyn_p2_2.ℓdens,
+        dyn_p2_1.cacheℓdens == dyn_p2_2.cacheℓdens,
+        dyn_p2_1.τ == dyn_p2_2.τ])
 end
