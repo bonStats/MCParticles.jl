2e34096ad4c61c12824d5681053f727a422d9251
Joshua Bon <joshuajbon@gmail.com>
2020-09-27 23:01:59 +1000

    Added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,3 +31,22 @@ end
         stat_p1_1.x == stat_p1_2.x,
         stat_p1_1.ℓw == stat_p1_2.ℓw])
 end
+
+@testset "MCParticles.jl helper functions" begin
+    # MCParticles.safe_normalise
+    ulw = log.(vcat(repeat([1], 1000),[10000]))
+    nlw = ulw .- log(11000)
+    unsafe_sum_diff = abs(sum(exp.(nlw))- 1)
+    safe_sum_diff = abs(sum(exp.(MCParticles.safe_normalise(ulw))) - 1)
+    @test unsafe_sum_diff > safe_sum_diff
+end
+
+# @testset "MCParticles.jl accessor functions" begin
+#     f(x,t) = -sum(x) + t
+#     dyn_p1 = Particle([1.0,1.0], 1.0, f, 1.0)
+#     dyn_p2 = Particle([1.0,2.0], 0.5, f, 1.0)
+#     arr_dyn_p = [dyn_p1, dyn_p1, dyn_p2, dyn_p2]
+#     # weights
+#     weights(arr_dyn_p)
+#     #@test unsafe_sum_diff > safe_sum_diff
+# end
