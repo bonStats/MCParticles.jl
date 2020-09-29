79653199fab04327210f884e4e9db5a5c19e3c70
bonStats <joshuajbon@gmail.com>
2020-09-29 11:40:56 +1000

    Docs for mutate added

--- src/particle-mutate.jl
+++ src/particle-mutate.jl
@@ -1,9 +1,28 @@
+"""
+    mutate(p::StaticParticle{T}, x::T) where {T<:Any}
+
+Mutate a particle - update the location.
+
+- ```p``` Particle to update
+- ```x``` New location of particle
+
+"""
 function mutate(p::StaticParticle{T}, x::T) where {T<:Any}
 
     StaticParticle(x, p.ℓw)
 
 end
 
+"""
+    mutate(p::DynamicParticle{T}, x::T, τ::Real = p.τ) where {T<:Any}
+
+    Mutate a particle - update the location.
+
+    - ```p``` Particle to update
+    - ```x``` New location of particle
+    - ```τ``` New temperature (or index) for ℓdens (defaults to old temperature)
+
+"""
 function mutate(p::DynamicParticle{T}, x::T, τ::Real = p.τ) where {T<:Any}
     # recalculates cached density
     # logweight = true
